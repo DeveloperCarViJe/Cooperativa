35836e86268fabe2a40a560de5570b454d8d6091
hp <hp@DESKTOP-CG7LE4F>
2024-09-28 18:57:15 -0500

    imagenes

---
+++ SQL/Secuencias.sql
@@ -0,0 +1,8 @@
+---- crear secuencias ---
+CREATE SEQUENCE choferes_seq
+START WITH 1           -- Inicia con el valor 1
+INCREMENT BY 1         -- Incrementa en 1 cada vez que se utiliza
+NOCACHE;               -- No almacena en caché los valores de la secuencia
+---Secuencias creadas---
+choferes_seq --Secuencia de la tabla choferes
+usuarios_seq --Secuencia de la tabla usuarios
