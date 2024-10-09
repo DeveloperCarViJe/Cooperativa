eced97c98372fb9f928d98d4189e2f1d93c2a02e
hp <hp@DESKTOP-CG7LE4F>
2024-10-08 22:55:26 -0500

    tablas sql

--- SQL/TABLAS.sql
+++ SQL/TABLAS.sql
@@ -4,5 +4,20 @@ SELECT * FROM USUARIOS;
 DROP TABLE USUARIOS;
 delete from USUARIOS;
 commit;
+update usuarios
+set estado = 'A';
+--where id_user=81;
+commit;
 
+----Choferes----
+SELECT * FROM Choferes;
+delete from Choferes;
+DROP TABLE Choferes;
+commit;
+update Choferes
+set estado = 'A';
+commit;
 
+----Choferes----
+SELECT * FROM ImagenesChoferes;
+DROP TABLE ImagenesChoferes;
